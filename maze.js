097a53b522e8e0f2daff95a21be88c52f6b91039
Kevel Campbell <kevel_campbell@hotmail.com>
2016-10-14 13:06:37 -0500

    Exercise 3:
    Exercise 3: Alerts on successful completion of maze

--- maze.js
+++ maze.js
@@ -1,14 +1,27 @@
 window.onload = mouseover;
+var result = true; 
+
 function mouseover(){
+	var win =document.getElementById('end');
 	var border = document.querySelectorAll('div.boundary');
 	for (var i =0; i <5; i++) {
 		border[i].setAttribute('onmouseover','outOfBounds()');
 	}
+	win.setAttribute('onmouseover','end()');
 }
 
 function outOfBounds(){
+	result = false;
 	var border = document.querySelectorAll('div.boundary');
 	for (var i =0; i <5; i++) {
 		border[i].setAttribute('style','background-color:#ff8888');
 	}
+}
+
+function end() {
+    if(result) {
+    	alert("You win! :)");
+    } else {
+        alert("Sorry, you lost. :(");
+    }
 }
